35e5c0811933cdd98c1dfe07579c22f57dfd3931
Sad-Face-Man <@arpozdeev@mail.ru>
2025-05-24 23:39:16 +0300

    edit settings.py. Allowed all hosts to access.

--- market_3d/settings.py
+++ market_3d/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x0(7-=%j)_0@t!64^+^qzrzjv(jk&elr7o!fj1bsu79ia46$24
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
